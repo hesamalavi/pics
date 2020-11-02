18551e0ce2bf9c2996dd50693af1636ff69f0c82
Hesam Alavi <hesamalavi@Hesams-MacBook-Pro.local>
2020-11-03 07:13:08 +1100

    *

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,27 @@
-import React from 'react'
-
+import React from 'react';
 
 class SearchBar extends React.Component {
+    state = { term: '' };
     render() {
-        return <div>Search bar</div>;
+        return (
+            <div className="ui segment">
+                <form className="ui form">
+                    <div>
+                        <label>Image Search</label>
+                        <input
+                            type="text"
+                            value={this.state.term}
+                            onChange={(e) =>
+                                this.setState({
+                                    term: e.target.value,
+                                })
+                            }
+                        />
+                    </div>
+                </form>
+            </div>
+        );
     }
 }
 
-export default SearchBar
+export default SearchBar;
