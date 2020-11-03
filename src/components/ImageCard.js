3bb573a1d4411cadec3f46af0aacde3483eeb585
Hesam Alavi <hesamalavi@Hesams-MacBook-Pro.local>
2020-11-04 10:27:13 +1100

    finished styling

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -3,12 +3,24 @@ import React from 'react';
 export default class ImageCard extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = { spans: 0 };
         this.imageRef = React.createRef();
     }
+
+    componentDidMount() {
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    }
+    setSpans = () => {
+        const height = this.imageRef.current.clientHeight;
+        const spans = Math.ceil(height / 10);
+        this.setState({ spans });
+        // this.setState({ spans: spans}) the above is a shorthand es6 because the key and value name are the same
+    };
     render() {
         const { description, urls } = this.props.image;
         return (
-            <div>
+            <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
                 <img ref={this.imageRef} alt={description} src={urls.regular} />
             </div>
         );
